ce5bcc3fdecb8c99d5fc236b0b0375964faba06b
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2024-10-11 12:57:50 +0700

    add prod db

---
+++ prod/data_stores/mysql/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = "~>1.8"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.60"
+    }
+  }
+  backend "s3" {
+    key = "prod/data-stores/mysql/terraform.tfstate"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region  = "us-east-1"
+  profile = "cloudguru"
+}
+
+module "database" {
+  source               = "../../../modules/data-stores/mysql"
+  db_username          = var.db_password
+  db_password          = var.db_password
+  db_identifier_prefix = "db-prod-"
+  db_name              = "example"
+  db_instance_class    = "db.m5.large"
+  allocated_storage    = 20
+}
