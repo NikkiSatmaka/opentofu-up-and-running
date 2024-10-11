d90ebe996a60670d4cf448351147c646e35da995
Nikki Satmaka <30800020+NikkiSatmaka@users.noreply.github.com>
2024-10-11 12:16:35 +0700

    feat: use modules for webserver-cluster

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -7,7 +7,12 @@ terraform {
     }
   }
   backend "s3" {
-    key = "stage/services/webserver-cluster/terraform.tfstate"
+    profile        = "cloudguru"
+    bucket         = "opentofu-up-and-running-state-548315"
+    key            = "stage/services/webserver-cluster/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "opentofu-up-and-running-locks"
   }
 }
 
@@ -17,150 +22,22 @@ provider "aws" {
   profile = "cloudguru"
 }
 
-data "terraform_remote_state" "db" {
-  backend = "s3"
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
 
-  config = {
-    bucket  = "opentofu-up-and-running-state-462468"
-    key     = "stage/data-stores/mysql/terraform.tfstate"
-    region  = "us-east-1"
-    profile = "cloudguru"
-  }
+  cluster_name           = "webservers-stage"
+  db_remote_state_bucket = "opentofu-up-and-running-state-548315"
+  db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
+
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 2
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-resource "aws_launch_template" "example" {
-  image_id               = "ami-0866a3c8686eaeeba"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
-
-  user_data = base64encode(
-    templatefile(
-      "user-data.sh", {
-        server_port = var.server_port
-        db_address  = data.terraform_remote_state.db.outputs.address
-        db_port     = data.terraform_remote_state.db.outputs.port
-      }
-    )
-  )
-
-  # Required when using a launch template with an auto scaling group
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_autoscaling_group" "example" {
-  launch_template {
-    id = aws_launch_template.example.id
-  }
-  vpc_zone_identifier = data.aws_subnets.default.ids
-
-  target_group_arns = [aws_lb_target_group.asg.arn]
-  health_check_type = "ELB"
-
-  min_size = 2
-  max_size = 10
-
-  tag {
-    key                 = "Name"
-    value               = "opentofu-asg-example"
-    propagate_at_launch = true
-  }
-}
-
-resource "aws_security_group" "instance" {
-  name = "opentofu-example-instance"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_webserver_ipv4" {
-  security_group_id            = aws_security_group.instance.id
-  referenced_security_group_id = aws_security_group.alb.id
-  from_port                    = var.server_port
-  ip_protocol                  = "tcp"
-  to_port                      = var.server_port
-}
-
-resource "aws_security_group" "alb" {
-  name = "opentofu-example-alb"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_inbound_http" {
-  security_group_id = aws_security_group.alb.id
+resource "aws_vpc_security_group_ingress_rule" "allow_testing_inbound" {
+  security_group_id = module.webserver_cluster.alb_security_group_id
   cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
+  from_port         = 12345
   ip_protocol       = "tcp"
-  to_port           = 80
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_outbound_all" {
-  security_group_id = aws_security_group.alb.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-}
-
-resource "aws_lb" "example" {
-  name               = "opentofu-asg-example"
-  load_balancer_type = "application"
-  subnets            = data.aws_subnets.default.ids
-  security_groups    = [aws_security_group.alb.id]
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  # By default, return a simple 404 page
-  default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
-    }
-  }
-}
-
-resource "aws_lb_target_group" "asg" {
-  name     = "opentofu-asg-example"
-  port     = var.server_port
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 15
-    timeout             = 3
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-}
-
-resource "aws_lb_listener_rule" "asg" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 100
-
-  condition {
-    path_pattern {
-      values = ["*"]
-    }
-  }
-
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.asg.arn
-  }
+  to_port           = 12345
 }
